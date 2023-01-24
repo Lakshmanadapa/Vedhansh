440e399759d6303b3872f73a1e62cb188a7c19c0
Lakshmanadapa <laxmanadapa777@gmail.com>
2023-01-24 23:00:26 +0530

    jfrog artifactory configeration with jenkins declarative pipeline

--- jfrog artifactory.groovy
+++ jfrog artifactory.groovy
@@ -1,8 +1,5 @@
 pipeline {
-    agent { label 'PSPK' }
-    triggers {
-        cron('* * * * *')
-    }
+    agent { label 'VEDHA' }
     stages {
         stage ('vcs') {
             steps {
@@ -10,25 +7,20 @@ pipeline {
                 branch: 'main'
             }
         }
-        stage ('build packages') {
-            steps {
-             sh 'mvn package'
-            }
-        }
         stage ('Artifactory configuration') {
             steps {
                 rtMavenDeployer (
                     id: "MAVEN_DEPLOYER",
-                    serverId: "jfrog_id",
-                    releaseRepo: "qt-libs-release-local",
-                    snapshotRepo: "qt-libs-snapshot-local"
+                    serverId: "dev_id",
+                    releaseRepo: 'qt-libs-release-local',
+                    snapshotRepo: 'qt-libs-snapshot-local'
                 )
             }
         }
         stage ('Exec Maven') {
             steps {
                 rtMavenRun (
-                    tool: "MVN-3.6.3", // Tool name from Jenkins configuration
+                    tool: 'MVN-3.6.3', // Tool name from Jenkins configuration
                     pom: 'pom.xml',
                     goals: 'package',
                     deployerId: "MAVEN_DEPLOYER"
@@ -38,13 +30,18 @@ pipeline {
         stage ('Publish build info') {
             steps {
                 rtPublishBuildInfo (
-                    serverId: "jfrog_id"
+                    serverId: "dev_id"
                 )
+            }
+        }
+        stage ('build packages') {
+            steps {
+                sh 'mvn package'
             }
         }
     }
 }
+       
         
     
 
-  
